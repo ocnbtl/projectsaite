f9c45e5ffcacaa1843fb9e35572d0e799c3ca158
Ocean <oceanx1400@gmail.com>
2026-08-03 20:54:28 -0500

    Update footer attribution to Madagin

--- src/components/site/site-footer.tsx
+++ src/components/site/site-footer.tsx
@@ -29,7 +29,7 @@ export async function SiteFooter() {
             <Link href="/privacy">Privacy</Link>
             <Link href="/admin/login">Admin</Link>
           </div>
-          <p>Website by Sunderdas</p>
+          <p>Website by Madagin</p>
         </div>
       </div>
     </footer>
